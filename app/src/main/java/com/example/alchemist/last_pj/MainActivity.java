b37d47098d8277abb5e87507f9822f5e85f69c05
hjw <ggyoz@naver.com>
2018-06-09 15:02:37 +0900

    폰트 추가 리스트 화면 추가

--- app/src/main/java/com/example/alchemist/last_pj/MainActivity.java
+++ app/src/main/java/com/example/alchemist/last_pj/MainActivity.java
@@ -2,31 +2,64 @@ package com.example.alchemist.last_pj;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnSignin;
-    TextView textID, textPW;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+    public static final int RC_SIGN_IN = 1000;
+    private FirebaseAuth mFirebaseAuth;
+    private GoogleApiClient mGoogleAplClint;
+
+    TextView textsignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSignin = (Button) findViewById(R.id.btnSignin);
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-        textID = (TextView) findViewById(R.id.textID);
-        textPW = (TextView) findViewById(R.id.textPW);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/BMJUA.ttf");
-        textID.setTypeface(typeFace);
-        textPW.setTypeface(typeFace);
+        mGoogleAplClint = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-        btnSignin.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+
+       textsignin = (TextView) findViewById(R.id.textsignin);
+       // textPW = (TextView) findViewById(R.id.textPW);
+
+       // 폰트 설정
+
+       Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/BMJUA.ttf");
+       textsignin.setTypeface(typeFace);
+
+        // 회원가입 버튼 누를때 > 회원가입화면
+        /*btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), sign_inActivity.class);
@@ -34,6 +67,49 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }
-        });
+        });*/
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleAplClint);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }else{
+                Toast.makeText(this, "Google Sign-In faild", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
+                        }else{
+                            startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        }
+                    }
+                });
     }
 }
